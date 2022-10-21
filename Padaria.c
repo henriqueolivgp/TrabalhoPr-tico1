2f5b9d6445ccc4fac1c3a1e9bd818a8827666b74
h3nrique <henrique.o@ipvc.pt>
2022-10-21 14:00:33 +0100

    upload file

---
+++ Padaria.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+/*
+Implementações:
+1.	referencia de venda;
+2.	identificação de cliente
+4.	quantidades
+5. categorias de produtos ex(gelados, bebidas, confeitarias e padaria....)
+-----------------------------------------------------------------------------
+Requesitos e funcionalidades
+1.	Introduzir a informação sobre uma venda;
+2.	Listar informações sobre todas as vendas de um cliente;
+3.	Modificar o preço e/ou quantidade de vendas de um dado cliente;
+4.	Listar categorias, selecionar categoria e ver as vendas de cada categoria;
+5.	Calcualr quantidade de vendas e total ganho em vendas de uma determinada categoria selecionada;
+6.	Calcular a media dos preços das vendasd da padaria;
+7.	Apresentar a informação da(s) venda(s) preço mais baixo;
+8. 
+*/
+int main(){
+	setlocale(LC_ALL, "portuguese");
+	printf("Bem Vindo a padaria P90\n");//mensagem de boas vindas
+	printf("Insira o seu nome:");//pedir o nome da pessoa para guardar as informações
+}
